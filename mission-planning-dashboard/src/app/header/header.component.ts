7e5e0b84d321208833d0830cf60b278dc8375d71
milaguileras <milaguileras@gmail.com>
2020-12-21 22:42:20 -0300

    updated header code  to match studio 2 starter

--- mission-planning-dashboard/src/app/header/header.component.ts
+++ mission-planning-dashboard/src/app/header/header.component.ts
@@ -9,10 +9,16 @@ export class HeaderComponent implements OnInit {
 
   missionName: string = "Mars 2030";
   rocketName: string = "Plasma Max";
-
+  editingMission: boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  updateMission(updatedName: string) {
+    this.missionName = updatedName;
+    this.editingMission = false;
   }
 
 }
